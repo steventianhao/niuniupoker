7a1c3ac9da3f921a9ef0db3fc7c4bfbd78c3b018
erlangxk <erlang@viossolution.com>
2013-09-04 18:13:38 +0800

    remove the toppage handler, and reuse the cowboy_static handler to serve the / path.

--- src/niuniupoker_app.erl
+++ src/niuniupoker_app.erl
@@ -12,7 +12,11 @@
 start(_StartType, _StartArgs) ->
     Dispatch=cowboy_router:compile([
     	{'_',[
-    			{"/",toppage_handler,[]},
+    			{"/",cowboy_static,[
+                    {directory,{priv_dir,niuniupoker,[]}},
+                    {file,<<"html_ws_client.html">>},
+                    {mimetypes,{fun mimetypes:path_to_mimes/2,default}}
+                ]},
     			{"/websocket",ws_handler,[]},
     			{"/static/[...]",cowboy_static,[
     				{directory,{priv_dir,niuniupoker,[<<"static">>]}},
@@ -20,7 +24,7 @@ start(_StartType, _StartArgs) ->
     			]}
     	]}
     ]),
-    {ok,_} = cowboy:start_http(http,100,[{port,8080}],[{env,[{dispatch,Dispatch}]}]),
+    {ok,_} = cowboy:start_http(http,100,[{port,7788}],[{env,[{dispatch,Dispatch}]}]),
     niuniupoker_sup:start_link().
 
 stop(_State) ->
